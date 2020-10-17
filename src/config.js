2bf79ab1cdc1999621bcf2860efd9244daff3f1c
Kalin Ramsey <kalinramsey@gmail.com>
2020-10-17 11:14:48 -0700

    created migrations for group ecosystems

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'false-api-token',
+    DATABASE_URL: process.env.API_TOKEN || 'postgresql://dunder_mifflin@localhost/availy'
   }
