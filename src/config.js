95a5a28bbeefd8ee6610a60869b7980077a6e1b4
Sunny Lee <leesunny507@gmail.com>
2020-11-05 12:37:15 -0500

    added testing information and setup tests

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    API_TOKEN: process.env.API_TOKEN || 'false-api-token',
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL: process.env.DB_URL || 'postgresql://admin@localhost/availy',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://admin@localhost/availy',
+
+  /* API_TOKEN: process.env.API_TOKEN || 'false-api-token', */
+}
