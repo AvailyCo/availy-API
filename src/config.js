edf63aed725dec43a80f1407b9a754aab420f8f2
Sunny Lee <leesunny507@gmail.com>
2020-11-05 12:43:40 -0500

    removed extra config file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://admin@localhost/availy',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://admin@localhost/availy',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://admin@localhost/availy',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://admin@localhost/availy',
 
   /* API_TOKEN: process.env.API_TOKEN || 'false-api-token', */
 }
