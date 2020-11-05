667c01da8e64b3fe372c64e3b4b5c1f88f8384dc
Sunny Lee <leesunny507@gmail.com>
2020-11-04 19:28:23 -0500

    completed timezone service

---
+++ src/timezone/timezone-service.js
@@ -0,0 +1,29 @@
+const TimezoneService = {
+    getAllTimezones(knex) {
+        return knex.select('*').from('timezone');
+    },
+    getTimezoneById(knex, timezoneid) {
+        return knex.from('timezone').select('*').where('timezoneid', timezoneid).first();
+    },
+    insertTimezone(knex, newTimeZone) {
+        return knex
+            .insert(newTimeZone)
+            .into('timezone')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+    deleteTimezone(knex, timezoneid) {
+        return knex('timezone')
+            .where({ timezoneid })
+            .delete()
+    },
+    updateTimezone(knex, timezoneid, newTimezoneFields) {
+        return knex('timezone')
+            .where({ timezoneid })
+            .update(newTimezoneFields)
+    }
+}
+
+module.exports = FoldersService;
