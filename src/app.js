da34e011027f20ffbd7ae8166656291f1159c4be
Sunny Lee <leesunny507@gmail.com>
2020-11-28 13:55:35 -0500

    removed comment

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use('/api/contacts', contactsRouter);
 app.use('/api/timezones', timezoneRouter);
 app.use('/api/groups', groupsRouter);
 
-app.use('/api/events', eventsRouter); <= do this next
+app.use('/api/events', eventsRouter);
 app.use('/api/guests', guestsRouter);
 app.use('/api/hosts', hostsRouter);
 app.use('/api/users', usersRouter);
